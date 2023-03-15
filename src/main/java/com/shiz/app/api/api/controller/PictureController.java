123a09a34d0cf320bd6694c9545c7276e651d982
ShiZ <kacpergasiul16@gmail.com>
2023-03-15 18:34:17 +0100

    Caching added - i hope it works as intended.

--- src/main/java/com/shiz/app/api/api/controller/PictureController.java
+++ src/main/java/com/shiz/app/api/api/controller/PictureController.java
@@ -2,16 +2,20 @@ package com.shiz.app.api.api.controller;
 
 import com.shiz.app.api.service.PictureService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.MediaType;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RestController
 public class PictureController {
@@ -19,20 +23,33 @@ public class PictureController {
     private PictureService pictureService;
 
     @Autowired
-    public PictureController(PictureService pictureService){
+    public PictureController(PictureService pictureService) throws MalformedURLException {
         this.pictureService=pictureService;
     }
 
-    @GetMapping("/Picture")
-    public String getPicture(@RequestParam Integer id, Integer width, Integer height) throws IOException {
+
+
+
+
+
+    @ResponseBody
+    @RequestMapping(value = "/Picture", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+    @Cacheable("static")
+    public byte[] getPicture(@RequestParam Integer id, Integer width, Integer height) throws IOException {
         Image picture = pictureService.getPicture(id);
         BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics2D = resizedImage.createGraphics();
         graphics2D.drawImage(picture, 0, 0, width, height, null);
         graphics2D.dispose();
-        File outputfile = new File("static/main_resources/resizedimage.png");
+            File outputfile = new File("static/main_resources/resizedimage.png");
         ImageIO.write(resizedImage, "png", outputfile);
-        return "<img src=\"main_resources/resizedimage.png\" alt=\"image not found, check the path to the file\" width=\""+width+"\" height=\""+height+"\">";
+
+        URL url = new URL("http://localhost:8080/static/main_resources/resizedimage.png");
+        InputStream in = new BufferedInputStream(url.openStream());
+        return FileCopyUtils.copyToByteArray(in);
+
+
+       // return "<img src=\"static/main_resources/resizedimage.png\" alt=\"image not found, check the path to the file\" width=\""+width+"\" height=\""+height+"\">";
     }
 
 }
